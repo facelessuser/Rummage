86002a0ba452a568f8dc1e5bd88e68fe1b6e0e30
facelessuser <faceless.shop@gmail.com>
2015-09-16 22:10:25 -0600

    Fix name typo

--- rummage/rummage/version.py
+++ rummage/rummage/version.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 __app__ = "Rummage"
 __version__ = "0.3.0"
 __status__ = "Beta"
-__maintainers__ = [("Isaac Muses", "IsaacMuse@gmail.com")]
+__maintainers__ = [("Isaac Muse", "IsaacMuse@gmail.com")]
 __help__ = "https://github.com/facelessuser/Rummage/issues"
 __manual__ = "http://facelessuser.github.io/Rummage/"
