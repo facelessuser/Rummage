d852a5fde080e46ec58ea8a5b9ebde8faaa1b02e
facelessuser <faceless.shop@gmail.com>
2017-06-23 18:34:56 -0600

    Update tests to drop 3.3 and include 3.6

--- setup.py
+++ setup.py
@@ -94,7 +94,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
