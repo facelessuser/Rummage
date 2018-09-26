cf41ff0404fe45324e4b977b55137f2ceb619df5
Isaac Muse <faceless.shop@gmail.com>
2018-09-26 09:50:57 -0600

    Inlcude and check if internal docs are up to date (#283)
    * Inlcude and check if internal docs are up to date
    
    * Process files in order
    
    * Update docs to reflect latest changes
    
    * Remove unnecessary files
    
    * Disable mkdocs search plugin [internal docs]
    
    * Show import error for dependencies
    
    * Fix lint and set site url to 'file://.'

--- tools/gen_docs.py
+++ tools/gen_docs.py
@@ -1,6 +1,36 @@
 """Copy changelog."""
+from __future__ import unicode_literals
 import sys
+import os
 import subprocess
+import glob
+import hashlib
+try:
+    import mkdocs  # noqa
+    import pymdownx  # noqa
+except ImportError:
+    print(
+        '========================================================================================\n'
+        'gen_docs requires mkdocs and pymdown-extensions to be installed properly.\n'
+        'You can install requirements with the command `pip install -r requirements/docs.txt`.\n'
+        'Please reslove the issues and try again.\n'
+        '========================================================================================\n'
+    )
+    raise
+
+__version__ = '2.0.0'
+
+FILES_PATTERNS = [
+    'mkdocs-internal.yml',
+    'docs/src/**/*.md',
+    'docs/src/**/*.txt',
+    'docs/src/**/*.png',
+    'docs/src/**/*.gif',
+    'docs/src/**/*.html',
+    'docs/internal_theme/*.html',
+    'docs/internal_theme/*.css',
+    'docs/internal_theme/*.js'
+]
 
 MKDOCS_CFG = "mkdocs-internal.yml"
 MKDOCS_BUILD = "rummage/lib/gui/data/docs"
@@ -45,7 +75,7 @@ def console(cmd, input_file=None):
     return output[0].decode('utf-8')
 
 
-def build_internal_docs():
+def build_internal_docs(verbose=False, debug=False):
     """Build internal docs."""
 
     print('Building Docs...')
@@ -59,7 +89,66 @@ def build_internal_docs():
             ]
         )
     )
+    gen_hash(verbose, debug)
+
+
+def hash_files(verbose, debug):
+    """Hash the file list."""
+
+    found = []
+    h = hashlib.new('md5')
+    for pattern in FILES_PATTERNS:
+        for f in glob.iglob(pattern, recursive=True):
+            name = f.replace('\\', '/')
+            found.append(name)
+    if verbose:
+        print('FILES:')
+    for f in sorted(found):
+        if verbose:
+            print(f)
+        h.update(f.encode('ascii'))
+        with open(f, 'rb') as f:
+            h.update(f.read().replace(b'\r\n', b'\n'))
+    result = h.hexdigest()
+    print('HASH: ', result)
+    return result
+
+
+def gen_hash(verbose, debug):
+    """Generate hash."""
+
+    result = hash_files(verbose, debug)
+    with open(os.path.join(MKDOCS_BUILD, '.dochash'), 'w') as f:
+        f.write(result)
+    return 0
+
+
+def test_hash(verbose=False, debug=False):
+    """Test hash."""
+
+    with open(os.path.join(MKDOCS_BUILD, '.dochash'), 'r') as f:
+        original = f.read()
+
+    result = hash_files(verbose, debug)
+    match = result == original
+    if not match:
+        print("FAIL: Internal documents are outdated! Please update via \"python tools/gen_docs.py\"")
+    return int(not match)
 
 
 if __name__ == "__main__":
-    build_internal_docs()
+    import argparse
+
+    parser = argparse.ArgumentParser(prog="gen_docs", description='Internal document generator.')
+    # Flag arguments
+    parser.add_argument('--version', action='version', version=('%(prog)s ' + __version__))
+    parser.add_argument('--debug', action='store_true', default=False, help=argparse.SUPPRESS)
+    parser.add_argument('--verbose', '-v', action='store_true', default=False)
+    parser.add_argument('--test', '-t', action='store_true', default=False)
+    args = parser.parse_args()
+
+    if args.test:
+        code = test_hash(verbose=args.verbose, debug=args.debug)
+    else:
+        code = build_internal_docs(verbose=args.verbose, debug=args.debug)
+    sys.exit(code)
