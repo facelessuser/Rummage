a0a1492e159de1ef0b1f36f2f2b27aa1f1a8d5b1
facelessuser <faceless.shop@gmail.com>
2019-05-26 19:08:55 -0600

    Prep 4.6.0

--- rummage/lib/__meta__.py
+++ rummage/lib/__meta__.py
@@ -186,7 +186,7 @@ def parse_version(ver, pre=False):
 
 
 #   (major, minor, micro, release type, pre-release build, post-release build, development-release)
-__version_info__ = Version(4, 6, 0, 'beta', 2)
+__version_info__ = Version(4, 6, 0, 'final')
 __version__ = __version_info__._get_canonical()
 __app__ = "Rummage"
 __status__ = __version_info__[3]
