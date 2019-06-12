4dbc97ca1daed3c1234c973d05c59764acbd8432
facelessuser <faceless.shop@gmail.com>
2019-06-12 10:42:30 -0700

    Use FLP_SMALL to get small default buttons for file pickers

--- rummage/lib/gui/controls/file_picker.py
+++ rummage/lib/gui/controls/file_picker.py
@@ -10,7 +10,7 @@ class FilePickerCustomCtrl(wx.FilePickerCtrl):
     def __init__(
         self, parent, id=wx.ID_ANY, path=wx.EmptyString,  # noqa: A002
         message=wx.FileSelectorPromptStr, wildcard=wx.FileSelectorDefaultWildcardStr,
-        pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.FLP_DEFAULT_STYLE,
+        pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.FLP_DEFAULT_STYLE | wx.FLP_SMALL,
         validator=wx.DefaultValidator, name=wx.FilePickerCtrlNameStr
     ):
         """Initialization."""
@@ -22,10 +22,6 @@ class FilePickerCustomCtrl(wx.FilePickerCtrl):
 
         super().__init__(parent, id, path, message, wildcard, pos, size, style, validator, name)
 
-        btn = self.GetPickerCtrl()
-        btn.SetLabel(self.BROWSE)
-        self.set_exact_fit()
-
     def wildcard_override(self, wildcard):
         """Allow wildcard override."""
 
@@ -35,15 +31,6 @@ class FilePickerCustomCtrl(wx.FilePickerCtrl):
         """Translate strings."""
 
         self.MESSAGE = _("Select file")
-        self.BROWSE = _("...")
-
-    def set_exact_fit(self):
-        """Set exact fit button size."""
-
-        btn = self.GetPickerCtrl()
-        w = btn.GetTextExtent(btn.GetLabel())[0]
-        width = w + 2 + 2 * 6 + 4 * 1
-        btn.SetMinSize(wx.Size(width, btn.GetSize()[1]))
 
 
 class FilePickerAudioCtrl(FilePickerCustomCtrl):
@@ -64,4 +51,3 @@ class FilePickerAudioCtrl(FilePickerCustomCtrl):
         """Translate strings."""
 
         self.MESSAGE = _("Select audio file")
-        self.BROWSE = _("...")
