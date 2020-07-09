328c8240b54f01bc678da4b55f11b214af14bb82
facelessuser <faceless.shop@gmail.com>
2020-07-09 00:22:01 -0600

    Fix windows taskbar with a windows specific approach
    Don't use the macOS dock technique to get the proper Windows taskbar
    icon, but set the windows app id so the current frame icon will now
    be used.

--- rummage/lib/gui/dialogs/rummage_dialog.py
+++ rummage/lib/gui/dialogs/rummage_dialog.py
@@ -340,7 +340,7 @@ class RummageFrame(gui.RummageFrame):
             data.get_image('rummage_large.png').GetIcon()
         )
 
-        if util.platform() != "linux":
+        if util.platform() == "macos":
             self.tbicon = TaskBarIcon(self, "Rummage", data.get_image('rummage_large.png').GetIcon())
 
         self.encodings = util.get_encodings()
@@ -2231,7 +2231,7 @@ class RummageFrame(gui.RummageFrame):
         self.m_result_file_list.destroy()
         self.m_statusbar.tear_down()
         notify.destroy_notifications()
-        if util.platform() != "linux":
+        if util.platform() == "macos":
             self.tbicon.Destroy()
         event.Skip()
 
